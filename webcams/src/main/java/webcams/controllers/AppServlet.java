1184be10ac79b05c0461327abe600941ef219edc
adambabusek <adam.babusek@gmail.com>
2017-07-01 19:56:52 +0200

    AppServlet added

---
+++ webcams/src/main/java/webcams/controllers/AppServlet.java
@@ -0,0 +1,21 @@
+package webcams.controllers;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AppServlet extends HttpServlet {	
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+		resp.setContentType("text/html");
+		resp.getWriter().print("Dogcam!");
+	}
+
+	
+}
